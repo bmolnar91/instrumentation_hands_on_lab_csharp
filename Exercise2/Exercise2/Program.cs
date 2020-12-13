57117da7b07f46dbcb91d45b4428abb89be531f4
Balint Molnar <balintkilencvenegy@gmail.com>
2020-12-13 18:07:09 +0100

    Add code for Exercise 2

---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog myLog = new EventLog("X", "localhost", "Demo");
+            Trace.AutoFlush = true;
+            EventLogTraceListener myListener = new EventLogTraceListener(myLog);
+            Trace.WriteLine("This is a test");
+        }
+    }
+}
